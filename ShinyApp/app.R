de8db1f1228fc43b3b3064396e6feb1de76e4393
rainfallnz <tim.kerr@rainfall.nz>
2021-10-29 09:59:22 +1300

    changed background to NZTop and changed the stream labels

--- ShinyApp/app.R
+++ ShinyApp/app.R
@@ -5,13 +5,20 @@ library(htmltools)
 #r_colors <- rgb(t(col2rgb(colors()) / 255))
 #names(r_colors) <- colors()
 reprojected.data.WGS84 <- readRDS("Data/SpatialData.RDS")
-map <- leaflet::leaflet() %>% 
-  leaflet::addProviderTiles(providers$OpenStreetMap) %>%
+
+#Set the online map source to TOPO NZ maps
+NZTopo250 = 'http://tiles-a.data-cdn.linz.govt.nz/services;key=8ed417cc81ea45a0b92d597307229b80/tiles/v4/layer=52324/EPSG:3857/{z}/{x}/{y}.png'
+NZTopo50 = 'http://tiles-a.data-cdn.linz.govt.nz/services;key=8ed417cc81ea45a0b92d597307229b80/tiles/v4/layer=52343/EPSG:3857/{z}/{x}/{y}.png'
+
+map <- leaflet::leaflet() %>%
+  addTiles(urlTemplate = NZTopo250, options = providerTileOptions(maxZoom=14),attribution = "<a href=\"http://https://www.linz.govt.nz/\">LINZ</a>", group = "LINZ Topographic") %>%
+  addTiles(urlTemplate = NZTopo50, options = providerTileOptions(minZoom=14), group = "LINZ Topographic") %>%
+  #leaflet::addProviderTiles(providers$OpenStreetMap) %>%
   setView(lng=175.5,lat=-40.0,zoom=8) %>%
   addPolygons(data = reprojected.data.WGS84$SubZones, color = "black", weight = 3, fillColor = "transparent", label = ~htmlEscape(Zone_Code)) %>%
   addCircleMarkers(data = reprojected.data.WGS84$MeasurementSites, color = "red",label = ~htmlEscape(sID)) %>%
   addCircleMarkers(data = reprojected.data.WGS84$PointSourceSites, color = "black", label = ~htmlEscape(Site.Name)) %>%
-  addPolylines(data = reprojected.data.WGS84$RiverNetwork, color= "blue", label = ~htmlEscape(PrefixedLabel))
+  addPolylines(data = reprojected.data.WGS84$RiverNetwork, color= "blue", label = ~htmlEscape(Label))
 
 ui <- fluidPage(
   titlePanel("LWP"),
